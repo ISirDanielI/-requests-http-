45b601c322ec3991a8293b0d8abedcccaf1c36c9
Daniel Loginov <danil.loginov04@mail.ru>
2023-03-14 19:02:42 +0500

    Second commit

--- main.py
+++ main.py
@@ -5,29 +5,35 @@ url = 'https://akabab.github.io/superhero-api/api'
 hero_list = ['Hulk','Captain America','Thanos']
 num = 1
 i = 0
-resp = requests.get(url + f'/id/{num}.json')
+resp = requests.get(url + f'/all.json')
 
-while i < len(resp.json()):
-    i += 1
-    num += 1
-    int_list = {}
-    for hero in hero_list:
-        if resp.json()['name'] == hero:
-            int_list.update({resp.json()['name'] : resp.json()['powerstats']['intelligence']})
+for hero in hero_list:
+    for i in resp.json():
+        if hero in i['name']:
+            hero_int = {}
+            hero_int.update({ i['name'] : i['powerstats']['intelligence']})
+
+def hero_vers():
+    if hero_int['Hulk'] > hero_int['Captain America'] and hero_int['Hulk'] > hero_int['Thanos']:
+        print('Самый высокий уровень интелекта у Халка')
+    elif hero_int['Captain America'] > hero_int['Hulk'] and hero_int['Captain America'] > hero_int['Thanos']:
+        print('Самый высокий уровень интелекта у Капитана Америки')
+    elif hero_int['Thanos'] > hero_int['Captain America'] and hero_int['Thanos'] > hero_int['Hulk']:
+        print('Самый высокий уровень интелекта у Таноса')
+    else:
+        print('Ошибка')
+
+print(hero_vers())
 
 host = 'https://cloud-api.yandex.net:443'
 
 class YaUploader:
     def __init__(self, token: str):
         self.token = token
-
-    def upload(self, file_path: str):
-        self.file_path = file_path
-        file_path = 'E:\Photoshop Galery\Kulturniy_zaba.png'
+    def upload(self):
         uri = 'v1/disk/resources/'
-        requests_url = self.file_path + uri
-        response = requests.put(requests_url)
-
+        file = {'photo':open('test.png')}
+        requests.post(uri, files = file)
 
 if __name__ == '__main__':
     path_to_file = 'v1/disk/resources/Kulturniy_zaba.png'
